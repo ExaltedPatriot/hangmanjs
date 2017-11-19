f1fa62fca28479ace77276926f07b94eae56d15a
Fagerstrom <Zach.Fagerstrom@QualityTech.local>
2017-11-19 08:36:06 -0700

    first full push

---
+++ hangmanwords.js
@@ -0,0 +1,32 @@
+/*
+ * File: hangmanwords.js
+ * ----------------------
+ * This file contains the words that are used to play hangman
+ *
+ * TODO: implement as different datatype(s) and/or as external source
+ */
+
+
+/*
+class HangmanWord {
+  constructor() {
+    this.name =
+  }
+
+}
+*/
+
+
+export function pickWord(){
+  let word = listOfWords[ Math.random() * listOfWords.length]
+}
+let listOfWords['BOUY',
+'COMPUTER',
+'CONNOISSUER',
+'DEHYDRATE',
+'FUZZY',
+'HUBUB',
+'KEYHOLE',
+'QUAGMIRE',
+'SLITHER',
+'ZIRCON']
